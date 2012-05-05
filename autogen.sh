e3d54e95138fb7ae4a01679e65eaafa89e0b9753
Kan-Ru Chen <kanru@kanru.info>
2012-05-05 15:14:31 +0800

    Cleanup autotools.

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,8 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
-AUTORECONF_ARGS=-i
-if [ -d m4 ]; then
-    AUTORECONF_ARGS="$AUTORECONF_ARGS -I m4"
+if [ ! -d m4 ]; then
+    mkdir m4
 fi
 
-autoreconf $AUTORECONF_ARGS
+autoreconf -i -I m4
